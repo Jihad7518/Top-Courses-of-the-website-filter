46a7ca51257e23b56705c3fdcdf8cf01ae1d8a2c
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-26 22:12:44 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-
+import React from "react";
+import Navbar from  "./components/Navbar";
+import Cards from "./components/Cards"
+import Filter from "./components/Filter"
+import { apiUrl, filterData  } from "./data";
+import { useState,useEffect } from "react";
+import Spinner from "./components/Spinner";
+import {toast} from "react-toastify";
