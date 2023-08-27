2fd16f4347c5b0654d3aa7f42a04aa03d0a32f25
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-27 21:58:47 +0600

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,3 +6,21 @@ const Cards = (props) => {
     let courses = props.courses;
     let category = props.category;
     const [likedCourses, setLikedCourses] = useState([]);
+    
+    function getCourses() {
+        if(category === "All") {
+            let allCourses = [];
+            Object.values(courses).forEach(array => {
+                array.forEach(courseData => {
+                    allCourses.push(courseData);
+                })
+            })
+            return allCourses;
+        }
+        else {
+            //main sirf specific categiry ka data array krunga  
+            return courses[category];      
+        }
+
+    }
+
