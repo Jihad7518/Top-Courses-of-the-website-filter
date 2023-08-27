0edb94249e04ca28751be4bf65e6faad9375beee
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-27 21:57:03 +0600

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,11 @@
+import React from 'react'
+import {FcLike,FcLikePlaceholder} from "react-icons/fc"
+import { toast } from 'react-toastify';
 
+const Card = (props) => {
+    let course = props.course;
+    let likedCourses = props.likedCourses;
+    let setLikedCourses = props.setLikedCourses;
+
+    function clickHandler() {
+        //logic
