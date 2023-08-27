bede11e77bffd524614fac5a1c47f2aff754cd05
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-27 21:59:01 +0600

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -23,4 +23,20 @@ const Cards = (props) => {
         }
 
     }
+    return (
+    <div className="flex flex-wrap justify-center gap-4 mb-4">
+      {
+        getCourses().map( (course) => (
+            <Card key={course.id} 
+            course = {course} 
+            likedCourses={likedCourses}
+            setLikedCourses={setLikedCourses}/>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Cards
+
 
