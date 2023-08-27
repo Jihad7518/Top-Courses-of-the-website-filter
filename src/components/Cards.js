792270255701d4fe4d2a5d8d0805b93ace11a24f
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-27 21:58:30 +0600

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,1 +1,8 @@
+import React from 'react'
+import Card from './Card';
+import { useState } from 'react';
 
+const Cards = (props) => {
+    let courses = props.courses;
+    let category = props.category;
+    const [likedCourses, setLikedCourses] = useState([]);
