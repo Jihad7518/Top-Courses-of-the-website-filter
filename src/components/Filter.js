fd77f1bf8836ae5982e0c8989bbd291077c86e40
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-28 19:06:06 +0600

    Update Filter.js

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,1 +1,10 @@
+import React from 'react'
 
+const Filter = (props) => {
+    let filterData = props.filterData;
+    let category = props.category;
+    let setCategory = props.setCategory;
+
+    function filterHandler(title) {
+        setCategory(title);
+    }
